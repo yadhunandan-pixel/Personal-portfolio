ff875a2ab79cc6ea62671f33b6ae4990a220b888
yadhunandan-pixel <yaduyadunandan71@gmail.com>
2025-09-04 19:19:23 +0530

    Update config and components for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/portfolio/',   // 👈 must match repo name exactly
+})
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio/"
 })
+*/
