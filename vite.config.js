ba3d2354188940f0bb5ed6693e962b07e29795e6
yadhunandan-pixel <yaduyadunandan71@gmail.com>
2025-09-04 23:17:01 +0530

    Fix vite base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',   // 👈 must match repo name exactly
+  base: "./",   // 👈 must match repo name exactly
 })
 
 
